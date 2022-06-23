3662c459c9c0786590bb6d1be9af06e53c34fb99
Scampy076 <104133496+Scampy076@users.noreply.github.com>
2022-06-23 11:02:46 +0200

    Initial commit
    index.html
    
    Delete .gitattributes
    
    sketch.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(800, 800);
+    background("grey");
+    angleMode(DEGREES);
+
+    noStroke();
+    
+    circle(200, 200, 100);
+    circle(200, 140, 70);
+    circle(200, 100, 40);
+    fill("black");
+    circle(195, 95, 5);
+    circle(205, 95, 5);
+    fill("orange");
+    circle(200, 100, 5);
+}
