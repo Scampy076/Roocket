636ff8cc08a70b9c1e3468d09f6b31c3651a8b9b
Scampy076 <104133496+Scampy076@users.noreply.github.com>
2022-06-23 11:26:52 +0200

    xPos en yPos
    afgewerkt voor verbetering

--- sketch.js
+++ sketch.js
@@ -3,55 +3,55 @@ function setup() {
     background("darkblue");
     angleMode(DEGREES);
          
-let xPos = 190;
-let yPos = 320;
+let xPos = 1200;
+let yPos = 750;
 
 noStroke();
-    circle(xPos, yPos, 100);
-    circle(xPos, yPos - 60, 70);
-    circle(xPos, yPos - 100, 40);
+circle(190, 320, 100);
+circle(190, 320 - 60, 70);
+circle(190, 320 - 100, 40);
     fill("black");
-    circle(xPos - 5, yPos - 105, 5);
-    circle(xPos + 5, yPos - 105, 5);
+    circle(190 - 5, 320 - 105, 5);
+    circle(190 + 5, 320 - 105, 5);
     fill("orange");
-    circle(xPos, yPos - 100, 5);
+    circle(190, 320 - 100, 5);
     stroke(51);
 
     fill("grey");
     
-    arc(1300, 752, 240, 240, 180, 0, HALF_PI);
+    arc(xPos + 100, yPos + 2, 240, 240, 180, 0, HALF_PI);
     
     fill(0,90)
         noStroke();
-        rect(1180, 750, 240, 250);
+        rect(xPos - 20, yPos, 240, 1000);
     
         stroke(51);
     
     fill("red");
     
-    triangle(1180, 750, 1420, 750, 1300, 970);
+    triangle(xPos - 20, yPos, xPos + 220, yPos, xPos + 100, yPos + 220);
     
     fill("orange");
     
-    triangle(1200, 750, 1400, 750, 1300, 940);
+    triangle(xPos, yPos, xPos + 200, yPos, xPos + 100, yPos + 190);
     
     fill("yellow");
     
-    triangle(1220, 750, 1380, 750, 1300, 910);
+    triangle(xPos + 20, yPos, xPos + 180, yPos, xPos + 100, yPos + 160);
     
     fill("white");
     
-    triangle(1200, 235, 1400, 235, 1300, 85);
-    rect(1200, 235, 200, 450);
+    triangle(xPos, yPos - 515, xPos + 200, yPos - 515, xPos + 100, yPos - 665);
+    rect(xPos, yPos - 515, 200, 450);
      
      fill("grey");
     
-     triangle(1265, 137.5, 1335, 137.5, 1300, 85);
-     circle(1300, 186, 80);
+     triangle(xPos + 65, yPos - 612.5, xPos + 135, yPos - 612.5, xPos + 100, yPos - 665);
+     circle(xPos + 100, yPos - 564);
     
      fill("lightblue");
     
-     circle(1300, 186, 70);
+     circle(xPos + 100, yPos - 564, 70);
     
      textFont("Ready Player One");
         let naam = "Stefan";
@@ -62,32 +62,32 @@ noStroke();
         let naam1 = "S";
         fill("lightblue")
         textSize(100);
-        text("S", 1260, 325);
+        text("S", xPos + 60, yPos - 425);
     
         let naam2 = "p";
         fill("lightblue")
         textSize(80);
-        text("p", 1271.5, 390);
+        text("p", xPos + 71.5, yPos - 360);
     
         let naam3 = "a";
         fill("lightblue")
         textSize(80);
-        text("a", 1271.5, 455);
+        text("a", xPos + 71.5, yPos - 295);
     
         let naam4 = "c";
         fill("lightblue")
         textSize(80);
-        text("c", 1271.5, 520);
+        text("c", xPos + 71.5, yPos - 230);
     
         let naam5 = "e";
         fill("lightblue")
         textSize(80);
-        text("e", 1271.5, 585);
+        text("e", xPos + 71.5, yPos - 165);
     
         let naam6 = "X";
         fill("lightblue")
         textSize(100);
-        text("X", 1260, 660);
+        text("X", xPos + 60, yPos - 90);
     }
 
 
