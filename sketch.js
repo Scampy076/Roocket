9c59b10ca1e5b717f626064e29f5b1b87339f938
Scampy076 <104133496+Scampy076@users.noreply.github.com>
2022-06-23 11:12:56 +0200

    Update sketch.js
    raket toegevoegd

--- sketch.js
+++ sketch.js
@@ -1,28 +1,95 @@
 function setup() {
-    createCanvas(1500, 800);
-    background("black");
+    createCanvas(1500, 1000);
+    background("darkblue");
     angleMode(DEGREES);
+         
+let xPos = 190;
+let yPos = 320;
 
-    textFont("Ready Player One")
-    let naam = "Stefan";
-    fill("lightblue")
-   textSize(200);
-    text("Stefan", 100, 500);
+noStroke();
+    circle(xPos, yPos, 100);
+    circle(xPos, yPos - 60, 70);
+    circle(xPos, yPos - 100, 40);
+    fill("black");
+    circle(xPos - 5, yPos - 105, 5);
+    circle(xPos + 5, yPos - 105, 5);
+    fill("orange");
+    circle(xPos, yPos - 100, 5);
+    stroke(51);
 
-    noStroke();
+    fill("grey");
     
-    let xPos = 190;
-    let yPos = 320;
+    arc(1300, 752, 240, 240, 180, 0, HALF_PI);
     
-        circle(xPos, yPos, 100);
-        circle(xPos, yPos - 60, 70);
-        circle(xPos, yPos - 100, 40);
-        fill("black");
-        circle(xPos - 5, yPos - 105, 5);
-        circle(xPos + 5, yPos - 105, 5);
-        fill("orange");
-        circle(xPos, yPos - 100, 5);
+    fill(0,90)
+        noStroke();
+        rect(1180, 750, 240, 250);
+    
+        stroke(51);
+    
+    fill("red");
+    
+    triangle(1180, 750, 1420, 750, 1300, 970);
+    
+    fill("orange");
+    
+    triangle(1200, 750, 1400, 750, 1300, 940);
+    
+    fill("yellow");
+    
+    triangle(1220, 750, 1380, 750, 1300, 910);
+    
+    fill("white");
+    
+    triangle(1200, 235, 1400, 235, 1300, 85);
+    rect(1200, 235, 200, 450);
+     
+     fill("grey");
+    
+     triangle(1265, 137.5, 1335, 137.5, 1300, 85);
+     circle(1300, 186, 80);
+    
+     fill("lightblue");
+    
+     circle(1300, 186, 70);
+    
+     textFont("Ready Player One");
+        let naam = "Stefan";
+       textSize(200);
+        text("Stefan", 100, 500);
+        
+    
+        let naam1 = "S";
+        fill("lightblue")
+        textSize(100);
+        text("S", 1260, 325);
+    
+        let naam2 = "p";
+        fill("lightblue")
+        textSize(80);
+        text("p", 1271.5, 390);
+    
+        let naam3 = "a";
+        fill("lightblue")
+        textSize(80);
+        text("a", 1271.5, 455);
+    
+        let naam4 = "c";
+        fill("lightblue")
+        textSize(80);
+        text("c", 1271.5, 520);
+    
+        let naam5 = "e";
+        fill("lightblue")
+        textSize(80);
+        text("e", 1271.5, 585);
+    
+        let naam6 = "X";
+        fill("lightblue")
+        textSize(100);
+        text("X", 1260, 660);
+    }
 
-        fill("red")
-        triangle("1200, 500, 1400, 500, 1300, 300");
-        }
+
+
+
