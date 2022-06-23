aa783515961115607393c0c374f1f34b3dd781de
Scampy076 <104133496+Scampy076@users.noreply.github.com>
2022-06-23 11:10:09 +0200

    Update sketch.js
    work in progress...

--- sketch.js
+++ sketch.js
@@ -1,16 +1,28 @@
 function setup() {
-    createCanvas(800, 800);
-    background("grey");
+    createCanvas(1500, 800);
+    background("black");
     angleMode(DEGREES);
+
+    textFont("Ready Player One")
+    let naam = "Stefan";
+    fill("lightblue")
+   textSize(200);
+    text("Stefan", 100, 500);
 
     noStroke();
     
-    circle(200, 200, 100);
-    circle(200, 140, 70);
-    circle(200, 100, 40);
-    fill("black");
-    circle(195, 95, 5);
-    circle(205, 95, 5);
-    fill("orange");
-    circle(200, 100, 5);
-}
+    let xPos = 190;
+    let yPos = 320;
+    
+        circle(xPos, yPos, 100);
+        circle(xPos, yPos - 60, 70);
+        circle(xPos, yPos - 100, 40);
+        fill("black");
+        circle(xPos - 5, yPos - 105, 5);
+        circle(xPos + 5, yPos - 105, 5);
+        fill("orange");
+        circle(xPos, yPos - 100, 5);
+
+        fill("red")
+        triangle("1200, 500, 1400, 500, 1300, 300");
+        }
